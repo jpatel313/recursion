1579764bccf3916ec8ae9c0da6f76bc2d3ef65fb
Jay Patel <jcp313@gmail.com>
2016-10-27 11:31:02 -0400

    initial commit

---
+++ Recursion/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(Fact(5));
+        }
+
+
+
+        static int Fact(int x)  //4!=4*3! // x!=x*(x-1)!
+        { 
+           
+            if (x == 1)
+                return 1;
+
+            return x * Fact(x - 1);//Fact(x-1) allows returning the variable as input to run again
+
+
+        }
+
+
+
+
+    }
+}
